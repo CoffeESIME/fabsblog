1620c78382a6312a0c24ef7bcb14c6bcc7503d8f
FabianR3P <heromfabian@gmail.com>
2023-03-27 11:25:14 -0600

    cleaning code

--- FrontEnd/maps/src/components/main/map/mapComponent.js
+++ FrontEnd/maps/src/components/main/map/mapComponent.js
@@ -1,32 +1,35 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
 import "../../../App.css";
-import { Container } from 'react-bootstrap';
-import { geoData } from "../../../GPXFiles/ZacamulpaMarqueza";
-function MapRoutes({
-  zoom,
-  center,
-  routes,
-  
-}) {
-
-	const positions = geoData.features.map(p => [p.geometry.coordinates[1], p.geometry.coordinates[0]]);
+import { Container } from "react-bootstrap";
+function MapRoutes({ zoom, center, routes, colorRoute, scrollWheel = true }) {
   return (
     <Container>
       <MapContainer
-        center={[19.353536, -99.318017]}
-        zoom={9}
-        scrollWheelZoom={false}
-        style={{ height: "25rem", width: "100%" }}
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={scrollWheel}
+        style={{ height: "50rem", width: "100%" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Polyline
-	            pathOptions={{ fillColor: 'black', color: 'red' }}
-	            positions={positions}
-            />
+        {console.log('ele', routes)}
+        {routes.map((route) => (
+          
+          <Polyline
+            key={route.id}
+            pathOptions={{ color: "red" }}
+            positions={route.route}
+          />
+        ))}
       </MapContainer>
     </Container>
   );
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fabsblog
 Here I will put all the code that I've working for personal development
+For the maps the project will need to use node 16.10.0 version
